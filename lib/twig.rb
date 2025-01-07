62dcc4e7a02bbadfa7c361a52b0e52e425a90fcc
Craig Blanchette <craig.blanchette@gmail.com>
2025-01-06 22:08:23 -0500

    Load base classes before loading the entire directory

--- lib/twig.rb
+++ lib/twig.rb
@@ -1,25 +1,7 @@
 require 'digest'
 
-require_relative 'twig/environment'
-require_relative 'twig/compiler'
-require_relative 'twig/lexer'
-require_relative 'twig/source'
-require_relative 'twig/template'
-require_relative 'twig/token'
-require_relative 'twig/token_stream'
-require_relative 'twig/parser'
-require_relative 'twig/token_parser/base'
-require_relative 'twig/extension_set'
-require_relative 'twig/extension/base'
-require_relative 'twig/callable'
-require_relative 'twig/twig_filter'
-require_relative 'twig/loader/base'
-require_relative 'twig/node/base'
-require_relative 'twig/expression_parser'
-require_relative 'twig/node/expression/expression'
-require_relative 'twig/node/expression/binary/binary'
-
 %w[
+  .
   extension
   loader
   node
@@ -27,7 +9,11 @@ require_relative 'twig/node/expression/binary/binary'
   node/expression/binary
   token_parser
 ].each do |directory|
-  Dir[__dir__ + "/twig/#{directory}/*.rb"].each do |file|
+  directory = __dir__ + "/twig/#{directory}/"
+  require directory + 'base.rb' if File.file?(directory + 'base.rb')
+
+  Dir[directory + '*.rb'].each do |file|
+    next if File.basename(file).inspect == 'base.rb'
     require file
   end
 end
