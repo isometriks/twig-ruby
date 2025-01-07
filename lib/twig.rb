5d3c7913238b8268fd5f0657f88f258532472f1e
Craig Blanchette <craig.blanchette@gmail.com>
2025-01-06 20:28:55 -0500

    Simplify some loading

--- lib/twig.rb
+++ lib/twig.rb
@@ -7,38 +7,30 @@ require_relative 'twig/source'
 require_relative 'twig/template'
 require_relative 'twig/token'
 require_relative 'twig/token_stream'
-
 require_relative 'twig/parser'
 require_relative 'twig/token_parser/base'
-require_relative 'twig/token_parser/block'
-
 require_relative 'twig/extension_set'
 require_relative 'twig/extension/extension'
-require_relative 'twig/extension/core_extension'
-
 require_relative 'twig/callable'
 require_relative 'twig/twig_filter'
-
 require_relative 'twig/loader/base'
-require_relative 'twig/loader/array_loader'
-
 require_relative 'twig/node/node'
-require_relative 'twig/node/nodes'
-require_relative 'twig/node/block'
-require_relative 'twig/node/block_reference'
-require_relative 'twig/node/empty_node'
-require_relative 'twig/node/module_node'
-require_relative 'twig/node/print_node'
-require_relative 'twig/node/text_node'
-
 require_relative 'twig/expression_parser'
 require_relative 'twig/node/expression/expression'
-require_relative 'twig/node/expression/call'
-require_relative 'twig/node/expression/constant'
-require_relative 'twig/node/expression/filter'
-require_relative 'twig/node/expression/name'
 require_relative 'twig/node/expression/binary/binary'
-require_relative 'twig/node/expression/binary/concat_binary'
+
+%w[
+  extension
+  loader
+  node
+  node/expression
+  node/expression/binary
+  token_parser
+].each do |directory|
+  Dir[__dir__ + "/twig/#{directory}/*.rb"].each do |file|
+    require file
+  end
+end
 
 module Twig
 end
