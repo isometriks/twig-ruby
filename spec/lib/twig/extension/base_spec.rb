f3691a2df89e812323ccfa8a3a59087f126ce092
Craig Blanchette <craig.blanchette@gmail.com>
2025-01-15 00:22:03 -0500

    Add test for base extension

---
+++ spec/lib/twig/extension/base_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'integration_shared_examples'
+
+RSpec.describe Twig::Extension::Base do
+  it 'provides default values' do
+    expect([{}, {}]).to eq(subject.operators)
+    expect({}).to eq(subject.filters)
+    expect([]).to eq(subject.helper_methods)
+    expect([]).to eq(subject.token_parsers)
+  end
+end
