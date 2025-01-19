3b40d85ee164f26e70d3c77c23335c1fdb85a98e
Craig Blanchette <craig.blanchette@gmail.com>
2025-01-19 09:39:15 -0500

    Move fixtures into spec and test filesystem loader

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,4 +10,19 @@ SimpleCov.start do
   enable_coverage :branch
 end
 
+module FixtureHelpers
+  # @return [File]
+  def file_fixture(path)
+    File.new(File.join(fixture_path, path), 'r')
+  end
+
+  def fixture_path
+    File.join(__dir__, 'fixtures')
+  end
+end
+
+RSpec.configure do |config|
+  config.include(FixtureHelpers)
+end
+
 require_relative '../lib/twig_ruby'
