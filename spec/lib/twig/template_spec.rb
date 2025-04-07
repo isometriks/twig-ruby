ec9bb1af2f232adabc5cb17c43bd6ed072ecdf0d
Craig Blanchette <craig.blanchette@gmail.com>
2025-04-07 11:17:21 -0400

    Convert other exception test to use shared example

--- spec/lib/twig/template_spec.rb
+++ spec/lib/twig/template_spec.rb
@@ -1,18 +1,14 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require 'integration_shared_examples'
 
 RSpec.describe Twig::Template do
-  let(:loader) { Twig::Loader::Array.new({ 'template.twig' => template }) }
-  let(:environment) { Twig::Environment.new(loader) }
-
   context 'when a block is missing' do
-    let(:template) { '{{ block("missing") }}' }
-
-    it 'raises an exception for a missing block' do
-      expect do
-        environment.load_template('template.twig').render
-      end.to raise_error(Twig::Error::Runtime, /Block 'missing' on template/)
+    it_behaves_like 'render_and_raise' do
+      let(:template) { '{{ block("missing") }}' }
+      let(:error) { Twig::Error::Runtime }
+      let(:message) { /Block 'missing' on template/ }
     end
   end
 end
