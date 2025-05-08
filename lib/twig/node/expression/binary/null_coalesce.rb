fb6fca43428d961311c1b15a2e43d3d8bfd90c3a
Craig Blanchette <craig.blanchette@gmail.com>
2025-05-07 21:55:47 -0400

    Fix null coalesce operator
    Doesn't actually work on filters because the left side needs to use a
    defined test which doesn't exist for filters.

--- lib/twig/node/expression/binary/null_coalesce.rb
+++ lib/twig/node/expression/binary/null_coalesce.rb
@@ -7,15 +7,35 @@ module Twig
         class NullCoalesce < Binary::Base
           include OperatorEscape
 
+          def initialize(left, right, lineno)
+            super
+
+            test = Test::Defined.new(left.dup, TwigTest.new('defined'), Node::Empty.new, left.lineno)
+
+            unless left.is_a?(Expression::BlockReference)
+              test = Binary::And.new(
+                test,
+                Unary::Not.new(
+                  Test::Null.new(left, TwigTest.new('null'), Node::Empty.new, left.lineno),
+                  left.lineno
+                ),
+                left.lineno
+              )
+            end
+
+            left.attributes[:always_defined] = true
+            nodes[:test] = test
+          end
+
           # @param [Compiler] compiler
           def compile(compiler)
             compiler.
               raw('((').
+              subcompile(nodes[:test]).
+              raw(') ? (').
               subcompile(nodes[:left]).
-              raw(' == nil) ? (').
-              subcompile(nodes[:right]).
               raw(') : (').
-              subcompile(nodes[:left]).
+              subcompile(nodes[:right]).
               raw('))')
           end
 
