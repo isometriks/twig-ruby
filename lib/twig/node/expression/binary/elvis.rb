ebd258de0bf89b050a8d0576fd263f514e82b807
Craig Blanchette <craig.blanchette@gmail.com>
2025-06-19 10:31:49 -0400

    Fix elvis binary due to escaping issues

--- lib/twig/node/expression/binary/elvis.rb
+++ lib/twig/node/expression/binary/elvis.rb
@@ -7,8 +7,22 @@ module Twig
         class Elvis < Binary::Base
           include OperatorEscape
 
-          def operator(compiler)
-            compiler.raw('||')
+          def initialize(left, right, lineno)
+            super
+
+            nodes[:test] = left.dup
+            left.attributes[:always_defined] = true
+          end
+
+          def compile(compiler)
+            compiler.
+              raw('((').
+              subcompile(nodes[:test]).
+              raw(') ? (').
+              subcompile(nodes[:left]).
+              raw(') : (').
+              subcompile(nodes[:right]).
+              raw('))')
           end
 
           def operand_names_to_escape
