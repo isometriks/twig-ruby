341607fb58ce46038c98984c48720d85a9381f64
Craig Blanchette <craig.blanchette@gmail.com>
2025-06-19 18:01:40 -0400

    Switch from Railtie to Engine and make configurable

---
+++ lib/twig/rails/config.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Twig
+  module Rails
+    class Config
+      def self.defaults
+        ActiveSupport::OrderedOptions.new.merge!({
+          root: ::Rails.root,
+          paths: %w[/ app/views/],
+          debug: ::Rails.env.development?,
+          allow_helper_methods: true,
+          cache: ::Rails.root.join('tmp/cache/twig').to_s,
+          charset: 'UTF-8',
+          strict_variables: true,
+          auto_reload: nil,
+          loader: lambda do
+            ::Twig::Loader::Filesystem.new(
+              current.root,
+              current.paths
+            )
+          end,
+        })
+      end
+
+      def self.current
+        self.configuration ||= defaults
+      end
+
+      class_attribute :configuration
+    end
+  end
+end
---
+++ lib/twig/rails/renderer.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+module Twig
+  module Rails
+    class Renderer
+      def call(template, source)
+        <<~TEMPLATE
+          ::Twig.
+            environment.
+            load("#{template.short_identifier}").
+            render(
+              local_assigns,
+              call_context: self,
+              output_buffer: Twig::OutputBuffer.new(@output_buffer)
+            )
+
+          @output_buffer
+        TEMPLATE
+      end
+
+      def translate_location(spot, _backtrace_location, source)
+        exception = $ERROR_INFO
+
+        return nil unless exception.is_a?(::ActionView::Template::Error)
+
+        twig_exception = exception.cause
+
+        return nil unless twig_exception.is_a?(::Twig::Error::Base)
+
+        lineno = twig_exception.lineno
+        lineno = 1 if lineno == -1
+
+        spot[:script_lines] = twig_exception.source_context&.code&.lines || source.lines
+        spot[:first_lineno] = spot[:last_lineno] = lineno
+        spot[:first_column] = spot[:last_column] = 0
+
+        spot
+      end
+    end
+  end
+end
