060a6273723e212f90cfa1649fa06cbd967c33dd
Craig Blanchette <craig.blanchette@gmail.com>
2025-07-21 21:17:58 -0400

    Change AutoHash keys to be more like PHP
    If you add an integer key, or the hash already has one, we start
    incrementing up from that instead of using the current length of the
    hash

--- lib/twig/auto_hash.rb
+++ lib/twig/auto_hash.rb
@@ -4,7 +4,7 @@ module Twig
   class AutoHash < Hash
     def add(*values)
       values.each do |value|
-        self[length] = value
+        self[next_key] = value
       end
 
       self
@@ -12,6 +12,12 @@ module Twig
 
     def <<(*values)
       add(*values)
+    end
+
+    private
+
+    def next_key
+      (keys.filter { |key| key.is_a?(Integer) }.max || -1) + 1
     end
   end
 end
