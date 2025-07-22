6741dfe73221d70e544691be947f4e490e856d85
Craig Blanchette <craig.blanchette@gmail.com>
2025-07-22 16:09:09 -0400

    Change how enumerable filters work

---
+++ lib/twig/runtime/enumerable_hash.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module Twig
+  module Runtime
+    class EnumerableHash
+      include Enumerable
+
+      MISSING_KEY = "__§__missing_key_#{rand}".freeze
+
+      private_constant :MISSING_KEY
+
+      def self.from(object)
+        if object.is_a?(Array)
+          AutoHash.new.add(*object)
+        elsif object.is_a?(Hash)
+          object.to_h
+        else
+          new(object)
+        end
+      end
+
+      def initialize(wrapped)
+        @wrapped = wrapped
+      end
+
+      def each(...)
+        key = 0
+        @wrapped&.each do |k, v = MISSING_KEY| # rubocop:disable Style/HashEachMethods
+          if v == MISSING_KEY
+            yield(key, k)
+            key += 1
+          else
+            yield(k, v)
+          end
+        end
+      end
+
+      def values
+        object.collect { |_, v| v }
+      end
+
+      def filter
+        self.class.new(super)
+      end
+    end
+  end
+end
