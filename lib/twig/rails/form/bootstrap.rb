0869ce6df16b0d1cc9669a68e23d1ad4619d2323
Craig Blanchette <craig.blanchette@gmail.com>
2025-07-29 22:24:13 -0400

    wip

--- lib/twig/rails/form/bootstrap.rb
+++ lib/twig/rails/form/bootstrap.rb
@@ -2,6 +2,10 @@ module Twig
   module Rails
     module Form
       class Bootstrap < Twig
+        def input_group(&)
+          twig_template.render_block('input_group', { blocked: -> { yield } })
+        end
+
         private
 
         def template_name
